31becb82b0d3d32274e336ea69adc72747d8f834
ojoseneto <joseneto@outlook.it>
2024-08-20 22:07:32 -0300

    Create calculadora.sh
    Script de Execução

---
+++ calculadora.sh
@@ -0,0 +1,11 @@
+!#/bin/bash
+
+#Esse Script executa a calculadora.py
+echo ''Iniciando o Script''
+
+#Atualiza os pacotes e instala o Python3
+sudo apt update
+sudo apt intall python3
+
+#Executa a calculadora
+python3 /home/jose/modulo1/python/calculadora.py
